b1c481985a48a5bd8b827d6c98f3102739b81bda
Kristoffer Berdal <web@flexd.net>
2012-03-01 17:57:30 +0100

    Info om oss

--- src/oblig1/Company.java
+++ src/oblig1/Company.java
@@ -1,13 +1,20 @@
-/*
- */
-package oblig1;
-
 /**
  *
  * @author Kristoffer Berdal <web@flexd.net>
  * @studnr 180212
- * @date Feb 28, 2012
+
+ * @author Even Augdal <even.augdal@gmail.com>
+ * @studnr 181091
+ * 
+ * @author Tommy Nyrud <s180487@stud.hioa.no>
+ * @studnr 180487
+ * 
+ * @date Feb 20, 2012
  */
+
+package oblig1;
+
+
 class Company extends AbstractOwner {
   protected int companyID; //Company number
   
